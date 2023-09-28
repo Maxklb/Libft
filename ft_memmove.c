b661e1d621dc3a26cd88005a0d9f5e340a34cb4f
Maxklb <themaxklb@gmail.com>
2023-09-28 18:41:23 +0200

    add memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 11:33:37 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/09/27 11:34:10 by makoch-l         ###   ########.fr       */
+/*   Updated: 2023/09/28 18:40:56 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 void *memmove(void *dest, const void *src, size_t n)
 {
+    size_t  i;
+    char    *dest2;
+    const char  *src2;
+
+    i = 0;
     
+    if (!src || !dest)
+        return (NULL);
+    if (src2 < dest2)
+    {
+        while (n)
+        {
+            dest2[n - 1] = src2[n - 1];
+            n--;
+        }
+    }
+    else
+    {
+        while (i < n)
+        {
+            dest2[i] = src2[i];
+            i++;
+        }
+    }
+    return (dest2);
 }
