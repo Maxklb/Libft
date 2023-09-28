e174f82569c9998b6948944032684bad3fc39f86
Maxklb <themaxklb@gmail.com>
2023-09-28 18:45:49 +0200

    fix ft name

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 22:16:35 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/09/26 19:11:21 by makoch-l         ###   ########.fr       */
+/*   Updated: 2023/09/28 18:44:05 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *memcpy(void *dest, const void *src, size_t n)
+void *ft_memcpy(void *dest, const void *src, size_t n)
 {
     size_t i;
     char    *dest2;
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,42 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/27 11:33:37 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/09/28 18:44:02 by makoch-l         ###   ########.fr       */
+/*   Created: 2023/09/28 18:41:45 by makoch-l          #+#    #+#             */
+/*   Updated: 2023/09/28 18:45:17 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-    size_t  i;
-    char    *dest2;
-    const char  *src2;
-
-    i = 0;
     
-    if (!src || !dest)
-        return (NULL);
-    if (src2 < dest2)
-    {
-        while (n)
-        {
-            dest2[n - 1] = src2[n - 1];
-            n--;
-        }
-    }
-    else
-    {
-        while (i < n)
-        {
-            dest2[i] = src2[i];
-            i++;
-        }
-    }
-    return (dest2);
 }
