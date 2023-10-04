24f55c195d4e9cbd2574ae9f5f93b3314e8f9b9b
Maxklb <themaxklb@gmail.com>
2023-10-04 18:24:45 +0200

    galere

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,32 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/28 18:41:45 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/10/04 18:23:11 by makoch-l         ###   ########.fr       */
+/*   Created: 2023/10/04 18:13:49 by makoch-l          #+#    #+#             */
+/*   Updated: 2023/10/04 18:16:10 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    int  i;
-    size_t  len;
-    
+    int i;
+    size_t  lent;
+
     i = 0;
     len = ft_strlen(src);
-    if (!dst || !src)
-        return (0);
-    while (dstsize > (i + 1) && src[i])
-    {
-        dst[i] = src[i];
-        i++;
-    }
-    if (dstsize > i)
-        dst[i] = '\0';
-    return (len);
+    if (!dst)
+        return (len);
 }
