83e03d72d0b6029636a8e0532f8399d7ca25b84f
Maxklb <themaxklb@gmail.com>
2023-11-06 16:03:55 +0100

    save

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 19:15:56 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/10/04 19:17:25 by makoch-l         ###   ########.fr       */
+/*   Updated: 2023/11/06 16:03:38 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ void    ft_putstr_fd(char *s, int fd)
 void    *ft_memset(void *s, int c, size_t n)
 void    *ft_memmove(void *dest, const void *src, size_t n)
 void    *ft_memcpy(void *dest, const void *src, size_t n)
+char    *ft_strchr(const char *string, int searchedChar)
 
 #endif
