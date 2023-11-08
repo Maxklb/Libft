6315ce7ae65b98bb3cc6f81127270867bdba335d
Maxklb <themaxklb@gmail.com>
2023-11-08 13:28:16 +0100

    save

---
+++ ft_strrchr.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 13:25:10 by makoch-l          #+#    #+#             */
+/*   Updated: 2023/11/08 13:27:48 by makoch-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *string, int searchedchar)
+{
+	char			*str;
+	unsigned char	char_searchedchar;
+}
