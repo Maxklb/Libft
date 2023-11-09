7f64236e00ce96b3f2b4d910e942045d26c9796d
Maxklb <themaxklb@gmail.com>
2023-11-09 12:15:44 +0100

    Add strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 10:45:00 by makoch-l          #+#    #+#             */
+/*   Updated: 2023/11/09 12:15:13 by makoch-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strncmp(const char *first, const char *second, size_t length)
+{
+	size_t	i;
+	int		diff;
+
+	i = 0;
+	diff = 0;
+	while (first[i] && second[i] && i < length)
+	{
+		if (first[i] != second[i])
+			return (first[i] - second[i]);
+		i++;
+	}
+	if (!diff && i < length)
+		diff = first[i] - second[i];
+	return (diff);
+}
+
+int main() 
+{
+	char s1[] = "test";
+	char s2[] = "testt";
+	
+	if (ft_strncmp(s1, s2, 4) < 0) 
+		printf("'%s' is lower than '%s'. \n", s1, s2);
+	if (ft_strncmp(s1, s2, 4) == 0)
+		printf("'%s' is equal to '%s'. \n", s1, s2);
+	if (ft_strncmp(s1, s2, 4) > 0) 
+		printf("'%s' is superior than '%s'. \n", s1, s2);
+	if (strncmp(s1, s2, 4) < 0) 
+		printf("'%s' is lower than '%s'. \n", s1, s2);
+	if (strncmp(s1, s2, 4) == 0)
+		printf("'%s' is equal to '%s'. \n", s1, s2);
+	if (strncmp(s1, s2, 4) > 0) 
+		printf("'%s' is superior than '%s'. \n", s1, s2);
+}
