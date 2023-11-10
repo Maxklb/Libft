eb96004df92b686cfc8937c9405dd0bb8aff8f9f
Maxklb <themaxklb@gmail.com>
2023-11-10 19:47:24 +0100

    t

--- libft.h
+++ libft.h
@@ -6,28 +6,28 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 19:15:56 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/11/08 12:18:18 by makoch-l         ###   ########.fr       */
+/*   Updated: 2023/11/10 19:47:11 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int ft_atoi(char *str)
-int ft_isalnum(int c)
-int ft_isalpha(int c)
-int ft_isascii(int c)
-int ft_isdigit(int c)
-int ft_isprint(int c)
-int ft_toupper(int c)
-int ft_lower(int c)
-int ft_strlen(char *str)
-void bzero(void *s, size_t n)
-void ft_putchar_fd(char c, int fd)
-void ft_putstr_fd(char *s, int fd)
-void *ft_memset(void *s, int c, size_t n)
-void *ft_memmove(void *dest, const void *src, size_t n)
-void *ft_memcpy(void *dest, const void *src, size_t n)
-char *ft_strchr(const char *string, int searchedChar)
+int ft_atoi(char *str);
+int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isascii(int c);
+int ft_isdigit(int c);
+int ft_isprint(int c);
+int ft_toupper(int c);
+int ft_lower(int c);
+int ft_strlen(char *str);
+void bzero(void *s, size_t n);
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void *ft_memset(void *s, int c, size_t n);
+void *ft_memmove(void *dest, const void *src, size_t n);
+void *ft_memcpy(void *dest, const void *src, size_t n);
+char *ft_strchr(const char *string, int searchedChar);
 
 #endif
