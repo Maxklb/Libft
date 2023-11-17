75db85779dd8cae2203076ad506835d118c3a9a9
Maxklb <themaxklb@gmail.com>
2023-11-17 16:23:23 +0100

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -5,57 +5,102 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/02 11:46:03 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/11/13 13:44:02 by makoch-l         ###   ########.fr       */
+/*   Created: 2023/11/15 18:32:21 by makoch-l          #+#    #+#             */
+/*   Updated: 2023/11/17 16:22:46 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_word_count(char const *s, char c)
+static size_t	ft_word_count(const char *s, char c)
 {
-	int	count;
-	int	word;
+	size_t	words;
+	int		state;
 
-	count = 0;
-	word = 0;
-	while (*s)
+	words = 0;
+	state = 1;
+	while (s && *s)
 	{
-		if (*s !c && !word)
+		if (*s == c)
+			state = 1;
+		else if (state == 1)
 		{
-			count++;
-			word = 1;
+			words++;
+			state = 0;
 		}
-		else if (*s == c)
-			word = 0;
 		s++;
 	}
-	return (count);
+	return (words);
 }
 
-static int	ft_word_lenght(char const *s, char c)
+static size_t	ft_word_length(const char *s, char c)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (s[i] != c && s[i])
+	while (s[i] && (s[i] != c))
 		i++;
 	return (i);
 }
 
-static char	*word_copy(char *src, int i)
+static	char	**free_tab(char **tab, size_t size)
 {
-	char			*dest;
+	size_t	i;
 
-	dest = malloc((i + 1) * sizeof(char *));
-	if (!dest)
-		return (NULL);
-	dest[i] = '\0';
-	while (i--)
-		dest[i] = src[i];
-	return (dest);
+	i = 0;
+	while (i < size)
+		free(tab[i++]);
+	free(tab);
+	return (0);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(const char *s, char c)
 {
+	char	**tab;
+	size_t	index;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	tab = malloc(sizeof(char *) * (ft_word_count(s, c) + 1));
+	if (!tab)
+		return (NULL);
+	index = 0;
+	while (ft_word_count(s, c))
+	{
+		while (*s == c)
+			s++;
+		tab[index] = malloc(sizeof(char) * (ft_word_length(s, c) + 1));
+		if (!tab[index])
+			return (free_tab(tab, index));
+		i = 0;
+		while (*s && (*s != c))
+			tab[index][i++] = *s++;
+		tab[index][i] = 0;
+		index++;
+	}
+	tab[index] = 0;
+	return (tab);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	const char *input_string = "test NOSNFS --459w5 949wm    d";
+	char **result;
+
+	result = ft_split(input_string, ' ');
+
+	if (result)
+	{
+		for (int i = 0; result[i] != NULL; i++)
+			printf("Mot : %s\n", result[i]);
+		for (int i = 0; result[i] != NULL; i++)
+			free(result[i]);
+		free(result);
+	}
+	else
+		printf("Marche pas\n");
+	return 0;
 }
