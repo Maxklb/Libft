59793634ea5b798dbb760d3f3cc8f6a8330be711
Maxklb <themaxklb@gmail.com>
2023-11-20 17:37:26 +0100

    substr almost done

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:56:16 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/11/20 16:21:36 by makoch-l         ###   ########.fr       */
+/*   Updated: 2023/11/20 17:36:35 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 char	*ft_substr(const char *source, unsigned int start, size_t length)
 {
+	char	*dest;
+	int		i;
 	
+	if (!source)
+		return (NULL);
+	if (length < ft_strlen(source))
+		dest = malloc(sizeof(char) * (ft_strlen(source) + 1));
+	else
+		dest = malloc(sizeof(char) * (length + 1));
+	if (!dest)
+		return (NULL);
 }
