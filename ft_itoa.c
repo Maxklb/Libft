c534d59091c755ca17955c2c4c3c7a38a469a086
Maxklb <themaxklb@gmail.com>
2023-11-22 18:53:45 +0100

    clean

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,22 +6,27 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 16:30:53 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/11/22 14:58:26 by makoch-l         ###   ########.fr       */
+/*   Updated: 2023/11/22 18:46:32 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
 
-static int	num_size(int num)
+static int	num_size(long num)
 {
 	int	i;
 
-	i = 0
+	i = 0;
+	if (num == 0)
+		i++;
 	if (num < 0)
-		num *= -1;
-	while (n != 0)	
 	{
-		n /= 10;
+		num = -num;
+		i++;
+	}
+	while (num > 0)
+	{
+		num /= 10;
 		i++;
 	}
 	return (i);
@@ -29,5 +34,27 @@ static int	num_size(int num)
 
 char	*ft_itoa(int n)
 {
-	
-}*/
+	char	*val;
+	long	nb;
+	size_t	length;
+
+	nb = n;
+	length = num_size(n);
+	val = malloc(sizeof(char) * (length + 1));
+	if (!val)
+		return (NULL);
+	val[length--] = 0;
+	if (n == 0)
+		val[0] = '0';
+	if (nb < 0)
+	{
+		val[0] = '-';
+		nb = -nb;
+	}
+	while (nb)
+	{
+		val[length--] = (nb % 10) + 48;
+		nb /= 10;
+	}
+	return (val);
+}
