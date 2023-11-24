61224c0892ce3bd2800f6708dd0c33cef76489b2
Maxklb <themaxklb@gmail.com>
2023-11-24 16:52:20 +0100

    libft clean

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,35 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 00:16:00 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/11/22 14:57:56 by makoch-l         ###   ########.fr       */
+/*   Updated: 2023/11/24 14:38:57 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	
-}*/
+	char	*dest;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) +1));
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dest[i +j] = s2[j];
+		j++;
+	}
+	dest[i + j] = 0;
+	return (dest);
+}
