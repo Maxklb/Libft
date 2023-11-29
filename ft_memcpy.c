53509b7852988ed14cca2e0cc2c3e26fd8a0357f
Maxklb <themaxklb@gmail.com>
2023-11-29 15:34:27 +0100

    clean

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 22:16:35 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/11/13 13:34:55 by makoch-l         ###   ########.fr       */
+/*   Updated: 2023/11/29 14:17:23 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t		i;
-	char		*dest2;
-	const char	*src2;
+	size_t	i;
+	char	*dest2;
+	char	*src2;
 
 	i = 0;
-	dest2 = dest;
-	src2 = src;
-	if (!src || !dest)
-		return (NULL);
+	dest2 = (char *) dest;
+	src2 = (char *) src;
 	while (i < n)
 	{
 		dest2[i] = src2[i];
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:56:16 by makoch-l          #+#    #+#             */
-/*   Updated: 2023/11/23 16:28:28 by makoch-l         ###   ########.fr       */
+/*   Updated: 2023/11/29 15:01:28 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,19 @@ char	*ft_substr(char const *source, unsigned int start, size_t len)
 	char	*dest;
 	size_t	i;
 
-	i = 0;
-	if (!source)
+	if (!source || (start >= ft_strlen(source)))
 		return (NULL);
-	if (len < ft_strlen(source))
-		dest = malloc(sizeof(char) * (ft_strlen(source) + 1));
-	else
-		dest = malloc(sizeof(char) * (len + 1));
+	if (len > ft_strlen(source) - start)
+		len = ft_strlen(source) - start;
+	dest = malloc(sizeof(char) * (len + 1));
 	if (!dest)
 		return (NULL);
-	if (start < ft_strlen(source))
+	i = 0;
+	while (i < len && source[start + i])
 	{
-		while (source[start + i] && i < len)
-		{
-			dest[i] = source[start + i];
-			i++;
-		}
-		dest[i] = 0;
+		dest[i] = source[start + i];
+		i++;
 	}
-	else
-		dest[0] = 0;
+	dest[i] = '\0';
 	return (dest);
 }
